6b2883ba5b183730d5d91a1e45333e66c0a4df0a
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2023-12-09 17:47:02 +0200

    add script loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,20 +3,58 @@ import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Conteiner } from './Container/Conteiner';
 import { Loader } from './Loader/Loader';
-// import { Button } from './Button/Button';
+import { fetchImages } from 'components/Services/api';
+import { Button } from './Button/Button';
 
 export class App extends React.Component {
   state = {
+    imagesData: [],
+    page: 1,
     loading: false,
+  };
+
+  async componentDidMount() {
+    try {
+      this.setState({ loading: true });
+      const images = await fetchImages({ per_page: 12 });
+      console.log(images);
+      this.setState({ imagesData: [...images] });
+    } catch (error) {
+      console.error();
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
+
+  async componentDidUpdate(_, prevState) {
+    if (prevState.page !== this.state.page) {
+      try {
+        this.setState({ loading: true });
+        const [...images] = await fetchImages({
+          per_page: 12,
+          page: this.state.page,
+        });
+        console.log(images);
+        this.setState({ imagesData: [...prevState.imagesData, ...images] });
+      } catch (error) {
+        console.error();
+      } finally {
+        this.setState({ loading: false });
+      }
+    }
+  }
+
+  handleLoadMore = () => {
+    this.setState(prevState => ({ page: prevState.page + 12 }));
   };
   render() {
     return (
       <div>
         <Searchbar />
         <Conteiner>
-          <ImageGallery />
+          <ImageGallery imagesData={this.state.imagesData} />
           {this.state.loading === true ? <Loader /> : null}
-          {/* <Button /> */}
+          <Button click={this.handleLoadMore} />
         </Conteiner>
       </div>
     );
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,46 +1,57 @@
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
-import { fetchImages } from 'components/Services/api';
 import React from 'react';
 import { CardsList } from './ImageGalleryStuled';
-import { Button } from 'components/Button/Button';
 
-export class ImageGallery extends React.Component {
-  state = {
-    imagesData: [],
-    page: 0,
-  };
+// export class ImageGallery extends React.Component {
+//   state = {
+//     imagesData: [],
+//     page: 0,
+//   };
 
-  async componentDidMount() {
-    const images = await fetchImages({ per_page: 12 });
-    console.log(images);
-    this.setState({ imagesData: [...images] });
-  }
+//   async componentDidMount() {
+//     const images = await fetchImages({ per_page: 12 });
+//     console.log(images);
+//     this.setState({ imagesData: [...images] });
+//   }
 
-  async componentDidUpdate(_, prevState) {
-    if (prevState.page !== this.state.page) {
-      const [...images] = await fetchImages({
-        per_page: 12,
-        page: this.state.page,
-      });
-      console.log(images);
-      this.setState({ imagesData: [...prevState.imagesData, ...images] });
-    }
-  }
+//   async componentDidUpdate(_, prevState) {
+//     if (prevState.page !== this.state.page) {
+//       const [...images] = await fetchImages({
+//         per_page: 12,
+//         page: this.state.page,
+//       });
+//       console.log(images);
+//       this.setState({ imagesData: [...prevState.imagesData, ...images] });
+//     }
+//   }
 
-  handleLoadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 12 }));
-  };
+//   handleLoadMore = () => {
+//     this.setState(prevState => ({ page: prevState.page + 12 }));
+//   };
 
-  render() {
-    return (
-      <div>
-        <CardsList>
-          {this.state.imagesData.map(item => {
-            return <ImageGalleryItem key={item.id} {...item} />;
-          })}
-        </CardsList>
-        <Button click={this.handleLoadMore} />
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <CardsList>
+//           {this.state.imagesData.map(item => {
+//             return <ImageGalleryItem key={item.id} {...item} />;
+//           })}
+//         </CardsList>
+//         <Button click={this.handleLoadMore} />
+//       </div>
+//     );
+//   }
+// }
+
+export const ImageGallery = ({ imagesData }) => {
+  return (
+    <div>
+      <CardsList>
+        {imagesData.map(item => {
+          return <ImageGalleryItem key={item.id} {...item} />;
+        })}
+      </CardsList>
+      {/* <Button click={this.handleLoadMore} /> */}
+    </div>
+  );
+};
